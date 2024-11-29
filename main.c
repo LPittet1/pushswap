169cdee5fee231510c56e02f51f75bb4a9c48dc7
Loic Pittet <lpittet@c2r4s2.42lausanne.ch>
2024-11-29 13:59:13 +0100

    fixed empty stack problem

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: lpittet <lpittet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/09 19:06:21 by lpittet           #+#    #+#             */
-/*   Updated: 2024/11/29 13:35:57 by lpittet          ###   ########.fr       */
+/*   Updated: 2024/11/29 13:56:38 by lpittet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,8 @@ int	main(int ac, char **av)
 		return (1);
 	stack_b = NULL;
 	stack_a = parser(av);
+	if (!stack_a)
+		return (1);
 	size = dblist_size(&stack_a);
 	choose_sort(&stack_a, &stack_b, size);
 	dblstdelete(stack_a);
