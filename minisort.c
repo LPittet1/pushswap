2789026076fb5aaa745907bf0ca39349d3ab5673
Loic Pittet <lpittet@c2r4s2.42lausanne.ch>
2024-11-29 13:26:16 +0100

    to delete

--- minisort.c
+++ minisort.c
@@ -6,60 +6,10 @@
 /*   By: lpittet <lpittet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/22 10:51:02 by lpittet           #+#    #+#             */
-/*   Updated: 2024/11/27 10:13:40 by lpittet          ###   ########.fr       */
+/*   Updated: 2024/11/29 13:25:52 by lpittet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pushswap.h"
 
-void	sort_3(t_stack **stack_a, t_stack **stack_b)
-{
-	if ((*stack_a)->prev->content > (*stack_a)->content
-		&& (*stack_a)->prev->content < (*stack_a)->next->content)
-	{
-		choose_op(stack_a, stack_b, "sa");
-		choose_op(stack_a, stack_b, "ra");
-	}
-	else if ((*stack_a)->content > (*stack_a)->next->content
-		&& (*stack_a)->content < (*stack_a)->prev->content)
-		choose_op(stack_a, stack_b, "sa");
-	else if ((*stack_a)->content < (*stack_a)->next->content
-		&& (*stack_a)->content > (*stack_a)->prev->content)
-		choose_op(stack_a, stack_b, "rra");
-	else if ((*stack_a)->next->content < (*stack_a)->content
-		&& (*stack_a)->next->content > (*stack_a)->prev->content)
-	{
-		choose_op(stack_a, stack_b, "sa");
-		choose_op(stack_a, stack_b, "rra");
-	}
-	else
-		choose_op(stack_a, stack_b, "ra");
-}
 
-void	sort_4(t_stack **stack_a, t_stack **stack_b)
-{
-	int	min;
-
-	min = find_min(stack_a, INT_MIN);
-	if ((*stack_a)->prev->content == min)
-		choose_op(stack_a, stack_b, "rra");
-	else
-		goto_val_a(stack_a, stack_b, min);
-	choose_op(stack_a, stack_b, "pb");
-	sort_3(stack_a, stack_b);
-	choose_op(stack_a, stack_b, "pa");
-}
-
-void	sort_5(t_stack **stack_a, t_stack **stack_b)
-{
-	int	min;
-
-	min = find_min(stack_a, INT_MIN);
-	if ((*stack_a)->prev->content == min)
-		choose_op(stack_a, stack_b, "rra");
-	else
-		goto_val_a(stack_a, stack_b, min);
-	choose_op(stack_a, stack_b, "pb");
-	sort_4(stack_a, stack_b);
-	choose_op(stack_a, stack_b, "pa");
-}
